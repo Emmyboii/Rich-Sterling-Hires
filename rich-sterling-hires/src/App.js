d505d1c029e11b7b6d6b58860710b9e3915c2c41
Olukoya Emmanuel <olukoyae01@gmail.com>
2025-05-14 11:10:06 +0100

    Commit

---
+++ rich-sterling-hires/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LandingPage from './Components/LandingPage';
+import './index.css'
+
+function App() {
+  return (
+    <div className="my-6 w-full">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
